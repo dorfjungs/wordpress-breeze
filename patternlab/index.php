52b5c11d9db4a3ae183226130bdc39101710dd1e
Esteban Ramirez <esteban@dorfjungs.com>
2019-11-27 17:59:05 -0600

    feat(patternlab): compile on page view

---
+++ patternlab/index.php
@@ -0,0 +1,10 @@
+<?php
+
+// if (DEVELOPMENT) {
+  shell_exec('php /var/www/app/vendor/pattern-lab/edition-twig-standard/core/console --generate');
+// }
+
+require('../_patternlab/index.html');
+
+
+
